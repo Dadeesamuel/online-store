330628d08ecbb27e1d09130baa1057d2da4e45c3
DADEE SAMUEL <dadeesamuel@gmail.com>
2021-11-05 15:30:23 +0100

    online marketing

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".col-3").click(function(){
+        window.location.href="reciept.html"
+    });
+    $(".col-4").click(function(){
+        window.location.href="about.html"
+    });
+});
