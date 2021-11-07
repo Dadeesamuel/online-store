7c6e42035569d0ea56074e4356f7cce4ffd46504
DADEE SAMUEL <dadeesamuel@gmail.com>
2021-11-07 13:28:14 +0100

    commenting

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+//business logic
+$(document).ready(function() {
+    $(".form-one").submit(function(event) {
+      $(".btn").click()
+      const lastName = $("#lastname").val().toUpperCase();
+      const firstName = $("#firstname").val().toUpperCase();
+      const otherName = $("#othername").val().toUpperCase();
+let fullName = (`${lastName} ${firstName} ${otherName}`);
+
+     $(".fullname").text(fullName);
+ 
+  
+      $("#story").toggle();
+
+      event.preventDefault();
+    });
+  });
+
+
+
+
+
 $(document).ready(function(){
     $(".col-3").click(function(){
         window.location.href="form.html"
